c2b815687126e592d02a2e794d82eff70137743b
Andrew Prince <90168409+AndrewEPrince@users.noreply.github.com>
2023-12-02 02:31:37 +0000

    20%

--- data.py
+++ data.py
@@ -6,6 +6,8 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import StandardScaler
+from sklearn.neighbors import KNeighborsClassifier
+
 
 
 
@@ -81,7 +83,7 @@ X_train = scaler.fit_transform(X_train)
 X_test = scaler.transform(X_test)
 
 # Create and train the model
-model = LogisticRegression(max_iter=1000)
+model = KNeighborsClassifier(n_neighbors=3)
 model.fit(X_train, y_train)
 
 # Check the accuracy of the model
